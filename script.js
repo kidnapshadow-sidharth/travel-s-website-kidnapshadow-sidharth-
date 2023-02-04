b2034e235ac78a8d7a212d55b58409a0df0b3f2b
kidnapshadow(sidharth) <107436833+kidnapshadow-sidharth@users.noreply.github.com>
2023-02-04 08:08:46 -0800

    sidharth
    kidnapshadow

---
+++ script.js
@@ -0,0 +1,6 @@
+const menubtn = document.querySelector('.menubtn')
+const navlink = document.querySelector('.navlinks') 
+menubtn.addEventListener('click',()=>{
+    navlink.classList.toggle('mobile-menu')
+})
+// code : kidnapshadow(sidharth)
